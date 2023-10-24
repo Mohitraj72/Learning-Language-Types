10ed0b547edb5504421218ef62b51bbce9fe5a86
Mohit Raj <146150589+Mohitraj72@users.noreply.github.com>
2023-10-24 16:45:40 +0530

    update

--- C Language By College Wallah/video 01/topic7.c
+++ C Language By College Wallah/video 01/topic7.c
@@ -4,7 +4,13 @@
 
 int main(){
   float x1 = 90; //x1 can be physics
-  float x2 = 91; //
+  float x2 = 91; // Maths Marks 
+  float x3 = 92; // english marks
+  float x4 = 87; // chemisty marks
+  float x5 = 96; // hindi marks 
+
+  float percent = (x1 + x2 + x3 + x4 + x5 )/5;
+  printf("Percentage of 5 Subject is : %f",percent);
 
 
 
