e08c2541c44d1b2dd83e0d715662c3fcb25cd377
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-11-19 19:45:54 +0530

    update

--- JavaScript By Code With Harry/Chapter 02/index.js
+++
@@ -1,9 +0,0 @@
-
-//! Operators in JavaScript 
-
-let a = 45;
-let b = 45;
-console.log("a + b ", a + b);
-console.log("a * b ", a * b);
-console.log("a - b ", a - b);
-console.log("a / b ", a / b);
