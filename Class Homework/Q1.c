dece1e0cc368b8a8240df2cc2ba1e0a6ecca2c64
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-12-20 20:10:11 +0530

    update

---
+++ Class Homework/Q1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+  int x;
+  printf("Enter you choice ");
+  scanf("%d", &x);
+  switch (x)
+  {
+  case 1:
+    printf("Hello World ");
+    case 2:
+    printf("Welcome ");
+    case 3:
+    printf("Bye");
+  
+  }
+  
+}
