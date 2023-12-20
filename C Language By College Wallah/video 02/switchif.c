43ceea0879cdc1795332f9397f689b043630aea4
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-12-20 09:45:13 +0530

    update

--- C Language By College Wallah/video 02/switchif.c
+++ C Language By College Wallah/video 02/switchif.c
@@ -1,32 +1,47 @@
+//
 #include <stdio.h>
-void main (){
-int a,b,c, choice;
-printf("Enter first no: ");
-scanf("%d", &a);
-printf("Enter second number: ");
-scanf("%d", &b);
+void main()
+{
+  int a, b, c;
+  char operator;
 
-printf(" 1---- Addition\n 2---- Subtraction\n 3---- Multiplication\n 4----division\n 5----Exit\n");
+  printf("Enter first no: ");
+  scanf("%d", &a);
+  printf("Enter second number: ");
+  scanf("%d", &b);
 
-printf("Enter your choice");
-scanf("%d", & choice);
-switch (choice){
-  case 1:
-  c = a+b;
-  break;
-  case 2:
-  c = a-b;
-  break;
-  case 3:
-  c = a*b;
-  break;
-  case 4:
-  c = a/b;
-  break;
-  case 5:
-  return;
+  // printf(" 1---- Addition\n 2---- Subtraction\n 3---- Multiplication\n 4----division\n 5----Exit\n");
+
+  printf("Enter your choice");
+  scanf("%c", &operator);
+
+  switch (operator)
+  {
+  case '+':
+    c = a + b;
+    printf("%d", c);
+    break;
+  case '-':
+    c = a - b;
+    printf("%d", c);
+    break;
+  case '*':
+    c = a * b;
+    printf("%d", c);
+    break;
+  case '/':
+    if (b != 0) {
+                c = a / b;
+                printf("Result: %.2f\n", c);
+            } else {
+                printf("Error: Division by zero!\n");
+            }
+            break;
+    case 5: 
+    return;
+
   default:
-  printf("invalid Choice");
-}
-printf ("%d", c);
+    printf("invalid Choice");
+  }
+  // printf("%d", c);
 }
