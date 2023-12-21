477911b197b35d8f675a93849190d9ba90b8d6a3
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-12-21 10:41:41 +0530

    update

---
+++ Class Homework/1st Class/Q7.c
@@ -0,0 +1,15 @@
+  // Volume of Sphere
+
+#include <stdio.h>
+void main()
+{
+  int volume, radius, height; // V=πr2h
+  
+  printf("Enter your radius ");
+  scanf("%d", &radius);
+
+  volume = (4.0 / 3.0) * 3.14159 * (radius * radius * radius);
+  
+  printf("%d", volume);
+
+}
