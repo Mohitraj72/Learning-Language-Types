61ee21f6e2d26e232fdaa54d76b288ca5a6fcb82
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2023-12-31 10:14:18 +0530

    update

--- C Language By College Wallah/video 03/Q9.c
+++ C Language By College Wallah/video 03/Q9.c
@@ -4,14 +4,13 @@
 int main()
 {
   int i, n;
-  int a =4;
+  int a = 4;
   printf("Enter your Number: ");
-  scanf("%d", & n);
+  scanf("%d", &n);
   for (i = 1; i <= n; i++)
   {
     printf("%d ", a);
-  a = a + 3;
-
+    a = a + 3;
   }
   return 0;
 }
