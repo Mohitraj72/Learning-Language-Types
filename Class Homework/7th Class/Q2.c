ba827a79bc1112370b1a75135ae66787d1f48e3c
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-01-05 00:58:31 +0530

    update

--- Class Homework/7th Class/Q2.c
+++ Class Homework/7th Class/Q2.c
@@ -3,17 +3,14 @@
 int main() {
     int n, i = 1, sum = 0;
 
-    // Input the value of n
     printf("Enter the value of n: ");
     scanf("%d", &n);
 
-    // Calculate the sum of the series using a while loop
     while (i <= n) {
         sum += i * i;
         i++;
     }
 
-    // Display the result
     printf("Sum of the series 1 + 4 + 9 + ... + %d^2 is: %d\n", n, sum);
 
     return 0;
