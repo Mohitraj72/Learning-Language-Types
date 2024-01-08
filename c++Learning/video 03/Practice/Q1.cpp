30f68a0220fce00b4f5624b2b130588569c2b3d2
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-01-08 21:38:28 +0530

    Update

---
+++ c++Learning/video 03/Practice/Q1.cpp
@@ -0,0 +1,14 @@
+// Two numbers are given, print their product
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int a = 10, b = 90;
+
+  int product = a * b;
+  cout<<product;
+    
+  return 0;
+}
---
+++ c++Learning/video 03/Practice/Q2.cpp
@@ -0,0 +1,17 @@
+// Two numbers are given a and b, print a-b
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int a = 90, b = 10;
+
+  int product = a - b;
+  cout<<product;
+    
+    
+    
+    
+  return 0;
+}
