d6afd8050871fd4dc9821558f77ee303b5aebd4c
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-01-15 11:37:28 +0530

    update

---
+++ c++Learning/video 04/Q5.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int number;
+  cout<<"Enter Your First Number: ";
+  cin>>number;
+  
+
+  if (number%2==0){
+    cout<<"Even Number";
+  } else {
+    cout<<"Odd Number";
+  }
+    
+    
+    
+    
+  return 0;
+}
