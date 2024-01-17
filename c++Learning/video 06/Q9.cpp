55975345bc99aab124cefa2c444c362872b68cfb
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-01-17 21:09:36 +0530

    update

---
+++ c++Learning/video 06/Q9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+  int i;
+  char j;
+
+  for (i = 1; i <= 5; i++)
+  {
+    for (j = 'a'; j <= 'e'; j++)
+    {
+      cout << j << " ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
