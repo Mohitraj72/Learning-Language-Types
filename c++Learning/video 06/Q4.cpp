fc302295df16d98f4c8f28fcb59d1b6104ef0303
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-01-17 13:37:46 +0530

    update

--- c++Learning/video 06/Q4.cpp
+++ c++Learning/video 06/Q4.cpp
@@ -6,12 +6,10 @@ int main(){
   int i,j;
   for(i=1; i<=5;i++){
     for(j=1; j<=5; j++){
-      cout<<i<<" ";
+      cout<<j<<" ";
     }
     cout<<endl;
   }
-    
-    
     
   return 0;
 }
--- c++Learning/video 06/Q6.cpp
+++ c++Learning/video 06/Q6.cpp
@@ -4,11 +4,11 @@ using namespace std;
 int main(){
 
   int i,j;
-  for(i=1; i<=5;i++){
+  for(i=1; i<=5; i++){ 
     for(j=1; j<=5; j++){
-      cout<<i<<" ";
+    cout<<j*j <<" ";
     }
-    cout<<endl;
+    cout<<"\n";
   }
     
     
