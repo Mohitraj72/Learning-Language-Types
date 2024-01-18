9cd281a9ed2c2607108eadee9c643981ce26e691
Mohitraj72 <146150589+Mohitraj72@users.noreply.github.com>
2024-01-18 21:00:51 +0530

    update

---
+++ c++Learning/video 10/Q1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+  int a,b;
+  cout<<"Enter Number a ";
+  cin>>a;
+
+  cout<<"Enter Number b ";
+  cin>>b;
+
+  // ==
+  // if(a==b){
+  //   cout<<"Yes";
+  // } else {
+  //   cout<<"No";
+  // }
+
+  if(a>=b){
+    cout<<"Yes";
+  } else {
+    cout<<"No";
+  }
+  
+    
+    
+    
+  return 0;
+}
